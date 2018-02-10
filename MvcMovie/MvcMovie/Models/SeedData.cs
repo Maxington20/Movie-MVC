02abef4e9adab46e425719a269d695c90a9b1058
Max Herrington <max.herrington@gmail.com>
2018-02-10 17:33:40 -0500

    Intial Commit to GitHub

---
+++ MvcMovie/MvcMovie/Models/SeedData.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+
+namespace MvcMovie.Models
+{
+    public static class SeedData
+    {
+		public static void Initialize(IServiceProvider serviceProvider)
+		{
+			using (var context = new MvcMovieContext(
+				serviceProvider.GetRequiredService<DbContextOptions<MvcMovieContext>>()))
+			{
+				if (context.Movie.Any())
+				{
+					return; //DB has been seeded
+				}
+
+				context.Movie.AddRange(
+					new Movie
+					{
+						Title = "When Harry Met Sally",
+						ReleaseDate = DateTime.Parse("1989"),
+						Genre = "Romantic Comedy",
+						Pice = 7.99M
+					},
+
+					new Movie
+					{
+						Title = "Ghostbusters",
+						ReleaseDate = DateTime.Parse("1984"),
+						Genre = "Comedy",
+						Pice = 8.99M
+					},
+
+					new Movie
+					{
+						Title = "Ghostbusters 2",
+						ReleaseDate = DateTime.Parse("1986"),
+						Genre = "Comedy",
+						Pice = 9.99M
+					},
+
+					new Movie
+					{
+						Title = "Star Wars",
+						ReleaseDate = DateTime.Parse("1977"),
+						Genre = "Adventure",
+						Pice = 10.99M
+					}
+				);
+				context.SaveChanges();
+			}
+		}
+    }
+}
